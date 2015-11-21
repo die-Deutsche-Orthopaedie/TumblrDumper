3a619ab3cb373807f3784cb03dd698b6e9a4a5a2
die♂Deutsche♂Orthopädie <amo@ludus.z0d.eu>
2015-11-22 03:12:10 +0800

    Update TumblrDumper.alt.sh
    it can't be perfectior

--- TumblrDumper.alt.sh
+++ TumblrDumper.alt.sh
@@ -3,32 +3,45 @@ blogname=$1
 mkdir $blogname
 cd $blogname
 
-#for archive in $(curl "http://$blogname.tumblr.com/archive" | sed 's/<\/li>/\n/g ' | grep -Eo "archive/[0-9]{4}/[0-9]*" )
 time="6666666666"
+mkdir 0x6d1b
+cd 0x6d1b
 
 until [ -z $time ]
 do
 	echo "in posts before $time: "
-	mkdir $time
-	cd $time
 
-	for link in $(curl "http://$blogname.tumblr.com/archive?before_time=$time" | grep -Eo "post/[0-9]*.*\"><div" | sed 's/"><div/\n/g ')
+	for link in $(curl "http://$blogname.tumblr.com/archive?before_time=$time" | sed 's/ /./g ' | grep -Eo "(post/[0-9]*.*\"><div|<h2.class=\"date\">.*</h2>)" | sed 's/"><div/\n/g ')
 	do
-		echo "    in $link: "
-		mkdir ${link##*/}
-		cd ${link##*/}
+		if [ -z "$(echo $link | grep "<h2.class=\"date\">.*</h2>")" ]
+		then
+			echo "    in $link: "
+			singlepost=${link#post\/}
+			singlepost=${singlepost//\//.}
+			mkdir $singlepost
+			cd $singlepost
 
-		for image in $(curl http://$blogname.tumblr.com/$link | sed 's/\/>/\n/g ' | grep -Eo "<meta property=\"og:image\" content=\".*.jpg" | sed 's/<meta property="og:image" content="//g ')
-		do
-			echo "        $image"
-			wget $image
-		done
+			for image in $(curl http://$blogname.tumblr.com/$link | sed 's/\/>/\n/g ' | grep -Eo "<meta property=\"og:image\" content=\".*.jpg" | sed 's/<meta property="og:image" content="//g ')
+			do
+				echo "        $image"
+				wget $image
+			done
 
-		echo
-		cd ..
+			echo
+			cd ..
+		else
+			cd ..
+			folder=${link#<h2.class=\"date\">}
+			folder=${folder%<\/h2>}
+			echo "photos will be stored in: $folder"
+			mkdir $folder
+			cd $folder 
+		fi
 	done
 	
 	echo
-	cd ..
 	time=$(curl "http://$blogname.tumblr.com/archive?before_time=$time" | grep -Eo "<a id=\"next_page_link\" href=\"\/archive\?before_time=[0-9]*\">" | grep -Eo "[0-9]*" )
 done 2> /dev/null
+
+cd ..
+rm -rf 0x6d1b
