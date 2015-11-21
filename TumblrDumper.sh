9c57c7c9a6cb5fbe46a5471dd555dd99e537269a
die♂Deutsche♂Orthopädie <amo@ludus.z0d.eu>
2015-11-22 00:55:05 +0800

    Update TumblrDumper.sh
    Editio Secunda. no post redirect detection. dump speed greatly improved.

--- TumblrDumper.sh
+++ TumblrDumper.sh
@@ -3,42 +3,28 @@ blogname=$1
 mkdir $blogname
 cd $blogname
 
-for archive in $(curl "http://$blogname.tumblr.com/archive" | sed 's/<\/a>/\n/g ' | grep -Eo "archive/[0-9]{4}/[0-9]*" )
+for archive in $(curl "http://$blogname.tumblr.com/archive" | sed 's/<\/li>/\n/g ' | grep -Eo "archive/[0-9]{4}/[0-9]*" )
 do
-	echo "in $archive:"
+	echo "in $archive: "
 	mkdir ${archive//\//.}
 	cd ${archive//\//.}
-	
-	for link in $(curl "http://$blogname.tumblr.com/$archive" | grep -Eo "http://$blogname.tumblr.com/post/[0-9]*")
-	# verified, no matter it set a title or not
+
+	for link in $(curl "http://$blogname.tumblr.com/$archive" | grep -Eo "post/[0-9]*.*\"><div" | sed 's/"><div/\n/g ')
 	do
-		echo $link
-		reallink=$(curl -Ls -o /dev/null -w %{url_effective} $link)
-		# reallink=$(curl -i $link | grep 'Location' | awk -F': ' '{print $2}' | tail -1)
-		# reallink=$(curl -i $link | grep "Location")
-		# reallink=${reallink//Location:/}
-		reallink=${reallink//#_=_/}
-		if [ -z "${reallink##*/}" ]
-		then
-			post=$reallink
-		else
-			post=${reallink##*/}
-		fi
-		mkdir $post
-		cd $post
-		
-		# for image in $(curl $reallink | sed 's/\/>/\n/g ' | grep -Eo "<meta property=\"og:image\" content=\"http://[0-9].*tumblr.*_1280.jpg" | sed 's/<meta property="og:image" content="//g ')
-		for image in $(curl $reallink | sed 's/\/>/\n/g ' | grep -Eo "<meta property=\"og:image\" content=\".*.jpg" | sed 's/<meta property="og:image" content="//g ')
+		echo "    in $link: "
+		mkdir ${link##*/}
+		cd ${link##*/}
+
+		for image in $(curl http://$blogname.tumblr.com/$link | sed 's/\/>/\n/g ' | grep -Eo "<meta property=\"og:image\" content=\".*.jpg" | sed 's/<meta property="og:image" content="//g ')
 		do
-			echo $image
+			echo "        $image"
 			wget $image
 		done
-		
+
+		echo
 		cd ..
 	done
 	
 	echo
 	cd ..
-done
-
-cd ..
+done 2> /dev/null
